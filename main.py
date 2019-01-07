9f89b8505889c984dfbf7d2b0cec13fa297d3e8b
HuayueZhang <xjtuzhy@outlook.com>
2019-01-07 19:54:37 +0800

    deprecate Variable

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ def main():
                             shuffle=True, num_workers=opt.workers)
 
     # ------------Set labels------------
-    real_I_label_D = torch.ones((opt.batch_size,),).long()
-    fake_I_label_D = torch.zeros((opt.batch_size,),).long()
-    fake_I_label_G = torch.ones((opt.batch_size,),).long()
+    real_I_label_D = torch.ones((opt.batch_size,), requires_grad=True)
+    fake_I_label_D = torch.zeros((opt.batch_size,), requires_grad=True)
+    fake_I_label_G = torch.ones((opt.batch_size,), requires_grad=True)
 
     # ---Fixed z input to eval/visualize---
-    z_sample = torch.randn((opt.batch_size, opt.input_nz, 1, 1))
+    z_sample = torch.from_numpy(np.random.uniform(-1, 1, size=(opt.batch_size , opt.input_nz, 1, 1)))
+    z_sample = z_sample.requires_grad_(requires_grad=True).float()
 
     if torch.cuda.is_available():
         z_sample = z_sample.cuda()
@@ -48,7 +49,9 @@ def main():
     for epoch in range(opt.max_epoch):
         for batch, real_I in enumerate(dataloader):
             # Prepare input data
-            z_G = torch.randn((opt.batch_size, opt.input_nz, 1, 1))
+            real_I = real_I.requires_grad_(requires_grad=True)
+            z_G = torch.from_numpy(np.random.uniform(-1, 1, size=(opt.batch_size , opt.input_nz, 1, 1)))
+            z_G = z_G.requires_grad_(requires_grad=True).float()
             if torch.cuda.is_available():
                 real_I = real_I.cuda()
                 z_G = z_G.cuda()
@@ -59,8 +62,8 @@ def main():
             fake_I_logits = net_D(fake_I.detach())
             # ****** detach阻断梯度继续反向计算，计算多余梯度，反正优化器里也不会优化之前的参数******
             real_I_logits = net_D(real_I)
-            loss_D = 0.5 * criterion_D(fake_I_logits, fake_I_label_D) + \
-                     0.5 * criterion_D(real_I_logits, real_I_label_D)
+            loss_D = 0.5 * criterion_D(fake_I_logits, fake_I_label_D.detach().long()) + \
+                     0.5 * criterion_D(real_I_logits, real_I_label_D.detach().long())
             # Backward
             optim_D.zero_grad()
             loss_D.backward()
@@ -70,7 +73,7 @@ def main():
             # Forward
             # fake_I = net_G(z_G)  # 此时net_G没有变，z_G和上一个fake_I都是一样的
             fake_I_logits = net_D(fake_I)
-            loss_G = criterion_G(fake_I_logits, fake_I_label_G)
+            loss_G = criterion_G(fake_I_logits, fake_I_label_G.detach().long())
             # Backward
             optim_G.zero_grad()
             loss_G.backward()
@@ -82,7 +85,7 @@ def main():
             fake_I_logits = net_D(fake_I)
             # ****** 此处fake_I没有加detach()，是因为更新G网络，梯度要从D到G一直反向计算到初始位置，******
             # ****** 然后在优化器里面只更新G的参数。计算D的梯度只是为了把loss反传到G ******
-            loss_G = criterion_G(fake_I_logits, fake_I_label_G)
+            loss_G = criterion_G(fake_I_logits, fake_I_label_G.detach().long())
             # Backward
             optim_G.zero_grad()
             loss_G.backward()
@@ -98,7 +101,7 @@ def main():
                 save_imgs(fake_I, os.path.join(opt.sample_dir,
                                                'train_epoch_%d_batch_%d.png' % (epoch, batch)))
                 fake_I_logits = net_D(fake_I)
-                loss_G = criterion_G(fake_I_logits, fake_I_label_G)
+                loss_G = criterion_G(fake_I_logits, fake_I_label_G.detach().long())
                 print('Eval loss %.4f' % loss_G.item())
 
         # Save model every epoch
